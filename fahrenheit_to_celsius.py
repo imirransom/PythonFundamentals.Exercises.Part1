a1d116305a11ff2f7a05950e0a1f92920d319bb4
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-07 14:44:13 -0500

    finished

--- fahrenheit_to_celsius.py
+++ fahrenheit_to_celsius.py
@@ -1,3 +1,3 @@
-fahrenheit = 32
-celsius = (fahrenheit - 32) * 5/9
+fahrenheit = 84
+celsius = (fahrenheit - 32) * 5//9
 print(f"{fahrenheit} degrees fahrenheit is equal to {celsius} degree(s) celsius")
