8550123ab7d8db4f72467f9da9dc3b482c76861d
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-07 13:56:30 -0500

    done first and full name

---
+++ first_name.py
@@ -0,0 +1,2 @@
+first_name = "Imir"
+print(first_name*500)
---
+++ full_name.py
@@ -0,0 +1,7 @@
+first_name = "Imir "
+# print(first_name*500)
+
+last_name = "Ransom"
+
+full_name = first_name + last_name
+print(full_name)
